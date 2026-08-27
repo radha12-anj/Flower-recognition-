347614644cc8b5ee3024215c6fcd02ea032bb4b1
radha12-anj <sanapalaanjana@gmail.com>
2026-08-27 16:28:26 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,1 +1,30 @@
-
+import warnings
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.applications.vgg19 import VGG19, preprocess_input
+from keras.applications.xception import Xception, preprocess_input
+from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
+from keras.applications.mobilenet import MobileNet, preprocess_input
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.preprocessing import image
+from keras.models import Model
+from keras.models import model_from_json
+from keras.layers import Input
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import glob
+import cv2
+import h5py
+import os
+import json
+import datetime
+import time
+model=config["model"]
+include_top=config["include_top"]
+weights=config["weights"]
+train_path=config["train_path"]
+features_path=config["features_path"]
+label_path=config["label_path"]
+results=config["results"]
+test_size 		= config["test_size"]
+model_path 		= config["model_path"]
