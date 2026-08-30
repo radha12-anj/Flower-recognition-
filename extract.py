c29485deac27fa654073b388ff1938093e337b20
radha12-anj <sanapalaanjana@gmail.com>
2026-08-30 19:39:33 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -28,3 +28,33 @@ label_path=config["label_path"]
 results=config["results"]
 test_size 		= config["test_size"]
 model_path 		= config["model_path"]
+if model=="vgg16":
+  base=VGG16(weights=weights)
+  basee=Model(input=base.input,output=base.get_layer("fc1").output)
+if model=="vgg19":
+  base=VGG16(weights=weights)
+  basee=Model(input=base.input,output=base.get_layer("fc1").output)
+if model=="resnet50":
+  base=ResNet50(weights=weights)
+  basee=Model(input=base.input,output=base.get_layer("flatten").output)
+if model=="inceptionv3":
+  base=InceptionV3(include_top=include_top,weights=weights,input_tensor=Input(shape=(229,229,3)))
+  basee=Model(input=base.input,output=base.get_layer('custom').output)
+  image_size=(299,299)
+if model=="inceptionresnetv2":
+  base=InceptionResNetV2(include_top=include_top,weights=weights,input_tensor=Input(shape=(299,299,3)))
+  basee=Model(input=base.input,output=base.get_layer('custom').output)
+  image_size=(299,299)
+if model=="mobilenet":
+  base=MobileNet(include_top=include_top,weights=weights,input_tensor=Input(shape=(299,299,3)))
+  basee=Model(input=base.input,output=base.get_layer('custom').output)
+  image_size=(299,299)
+if model=="xception":
+  base=Xception(weights=weights)
+  basee=Model(input=base.input,output=base.get_layer("custom").output)
+  image_size=(299,299)
+else:
+  base=None
+
+  
+  
